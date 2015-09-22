4931d02898d41612cab948c71da411c484fe948a
David Kirkby <dkirkby@uci.edu>
2015-09-22 16:17:04 -0700

    import all sub-modules when bossdata is imported

--- bossdata/__init__.py
+++ bossdata/__init__.py
@@ -4,3 +4,10 @@
 __author__ = 'bossdata developers'
 __email__ = 'dkirkby@uci.edu'
 __version__ = '0.2.7dev'
+
+import bossdata.bits
+import bossdata.meta
+import bossdata.path
+import bossdata.plate
+import bossdata.remote
+import bossdata.spec
